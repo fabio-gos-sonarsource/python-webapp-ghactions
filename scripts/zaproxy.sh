4d70b56b8849fc23a30acd9d93136633153eaece
Joshua Quek <124841129+joshua-quek-sonarsource@users.noreply.github.com>
2024-04-02 01:18:13 -0700

    Update zaproxy.sh

--- scripts/zaproxy.sh
+++ scripts/zaproxy.sh
@@ -2,6 +2,6 @@
 
 docker run --rm -v /Users/joshuaquek/local-code-repos/nodejs-sonar/.owasp-zap/reports:/zap/wrk/:rw \
     owasp/zap2docker-stable zap-baseline.py \
-    -t http://host.docker.internal:8081 \
+    -t http://host.docker.internal:65413 \
     -r testreport.html \
     -x testreport.xml
